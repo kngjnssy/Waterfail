8c436a2f426d1085b9a72d401a34c3208faa1cf1
kngjnssy <kingajanossy@gmail.com>
2021-05-08 20:20:25 +0200

    added mosfet and button

--- _waterfail_project_v1.ino
+++ _waterfail_project_v1.ino
@@ -12,11 +12,20 @@
 #define C   A2
 #define D   A3
 
+// need a button to set mosPin() HIGH when reading is low
+
 DHT dht11(DHTPIN11, DHTTYPE11);
 RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false);
 
 const int ledPin =  13;
-const int fanPin = 12;     
+const int fanPin = 12;    
+const int mosPin = 7;    
+const int buttonPin = 2;    
+
+int buttonState = 0;         // variable for reading the pushbutton status
+
+
+const int optimalHumidity = 40; 
 
 void setup() {
   Serial.begin(9600);
@@ -24,6 +33,13 @@ void setup() {
 
   pinMode(ledPin, OUTPUT);
   pinMode(fanPin, OUTPUT);
+  pinMode(mosPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+
+  digitalWrite(mosPin, 0);
+  digitalWrite(buttonPin, 1);
+
+
 
   matrix.begin();
 
@@ -35,8 +51,8 @@ void setup() {
   // delay(1000);
 
   matrix.fillRect(0, 0, 32, 32, matrix.Color333(0, 0, 0));
-  matrix.setCursor(2,2);    // start at top left, with one pixel of spacing
-  matrix.setTextSize(1);     // size 1 == 8 pixels high
+  matrix.setCursor(2,2);
+  matrix.setTextSize(1);     
   matrix.setTextWrap(false); 
   matrix.setTextColor(matrix.Color333(1, 5, 5));
   matrix.println("hmdty:");
@@ -44,26 +60,56 @@ void setup() {
 
   int humDHT11 = dht11.readHumidity();
   if (isnan(humDHT11)) {
-    matrix.setCursor(2, 12);    // start at top left, with one pixel of spacing
-    matrix.setTextSize(1);     // size 1 == 8 pixels high
-    matrix.setTextWrap(false); // Don't wrap at end of line - will do ourselves
+  matrix.setCursor(9, 12);    
+    matrix.setTextSize(1);    
+    matrix.setTextWrap(false); 
     matrix.setTextColor(matrix.Color333(7, 2, 2));
-    matrix.println("NO");
-    matrix.println("DATA");  
+    matrix.println("IDK!");  
   }
+  if ( humDHT11 < optimalHumidity ) {
+    // if it's TOO LOW
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(fanPin, HIGH);
 
-  if ( humDHT11 < 15 ) {
+
     matrix.setCursor(9, 12);    // start at top left, with one pixel of spacing
     matrix.setTextSize(1);     // size 1 == 8 pixels high
     matrix.setTextWrap(false); // Don't wrap at end of line - will do ourselves
     matrix.setTextColor(matrix.Color333(7, 2, 2));
     matrix.print(humDHT11);
     matrix.println("%");
+    delay(800);
+
     matrix.println("LOW!");  
-  } else {
-    matrix.setCursor(9, 12);    // start at top left, with one pixel of spacing
+    delay(2000);
+    matrix.fillRect(0, 0, 32, 32, matrix.Color333(0, 0, 0));
+    matrix.setCursor(2, 2);    // start at top left, with one pixel of spacing
     matrix.setTextSize(1);     // size 1 == 8 pixels high
     matrix.setTextWrap(false); // Don't wrap at end of line - will do ourselves
+    matrix.setTextColor(matrix.Color333(7, 2, 2));
+    matrix.println("plug");
+    matrix.println(" in");
+    matrix.println("vapor");  
+    delay(2000);
+    for (int drop = 0; drop < 31; drop++) {
+      matrix.fillRect(0, 0, 32, drop, matrix.Color333(0, 6, 6));
+      // delay(100);
+      int randomVertical = random(0, 32);
+      matrix.drawLine(randomVertical, random(0, 32) + drop, randomVertical, random(0, 32), matrix.Color333(0, 3, 3));
+      matrix.drawLine(randomVertical, random(0, 32) + drop, randomVertical, random(0, 32), matrix.Color333(0, 2, 4));
+      matrix.drawLine(randomVertical, random(0, 32) + drop, randomVertical, random(0, 32), matrix.Color333(0, 0, 0));
+      matrix.drawLine(randomVertical, random(0, 32) + drop, randomVertical, random(0, 32), matrix.Color333(0, 1, 4));
+      matrix.drawLine(randomVertical, random(0, 32) + drop, randomVertical, random(0, 32), matrix.Color333(0, 7, 7));
+    }
+    matrix.fillRect(0, 0, 32, 32, matrix.Color333(0, 0, 0));
+    // delay(1000);
+  } else {
+    // if it's good level
+    // digitalWrite(mosPin, 1);
+
+    matrix.setCursor(9, 12);    
+    matrix.setTextSize(1);     
+    matrix.setTextWrap(false); 
     matrix.setTextColor(matrix.Color333(0, 3, 7));
     matrix.print(humDHT11);
     matrix.println("%");
@@ -80,94 +126,84 @@ void setup() {
       matrix.drawLine(randomVertical, random(0, 32) + drop, randomVertical, random(0, 32), matrix.Color333(0, 1, 4));
       matrix.drawLine(randomVertical, random(0, 32) + drop, randomVertical, random(0, 32), matrix.Color333(0, 7, 7));
     }
+    // Ilde == screen is black
     matrix.fillRect(0, 0, 32, 32, matrix.Color333(0, 0, 0));
   }
-};
+}
 
 void loop() {
-  // float tempDHT11 = dht11.readTemperature();
-  // float humDHT11 = dht11.readHumidity();
-  // delay(500);
 
-  // if (isnan(tempDHT11))
+
+  buttonState = digitalRead(buttonPin);
+
+  if (buttonState == HIGH) {
+    Serial.println("BUTTON HIGH");
+
+  } else {
+    Serial.println("BUTTON LOW");
+    digitalWrite(mosPin, 1);    
+
+  }
+
+  int tempDHT11 = dht11.readTemperature();
+  int humDHT11 = dht11.readHumidity();
+  delay(500);
+
+  // if (isnan(humDHT11))
   // {
   //   Serial.println("Failed to read from DHT 11 sensor!");
   //   return;
   // }
 
-  // if ( humDHT11 < 45 ) {
-  //   digitalWrite(ledPin, HIGH);
-  //   digitalWrite(fanPin, HIGH);
-  //   Serial.print("DHT11 Temperature: ");
-  //   Serial.print(tempDHT11);
-  //   Serial.print(" \xC2\xB0");
-  //   Serial.print("C | ");
-  //   Serial.print("Humidity: ");
-  //   Serial.print(humDHT11);
-  //   Serial.println(" % - TOO LOW!");
-
-  //   matrix.setCursor(2, 5);    // start at top left, with one pixel of spacing
-  //   matrix.setTextSize(1);     // size 1 == 8 pixels high
-  //   matrix.setTextWrap(false); // Don't wrap at end of line - will do ourselves
-  //   matrix.setTextColor(matrix.Color333(7, 2, 2));
-  //   matrix.println("%");
-  //   matrix.println("OK");
-  //   delay(3000);
-
-  //   matrix.fillRect(0, 0, 32, 32, matrix.Color333(0, 0, 0));
+  if ( humDHT11 < optimalHumidity ) {
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(fanPin, HIGH);
 
 
-  // } else {
-  //   digitalWrite(ledPin, LOW);
-  //   digitalWrite(fanPin, LOW);
-  //   Serial.print("DHT11 Temperature: ");
-  //   Serial.print(tempDHT11);
-  //   Serial.print(" \xC2\xB0");
-  //   Serial.print("C | ");
-  //   Serial.print("Humidity: ");
-  //   Serial.print(humDHT11);
-  //   Serial.println(" % - optimal");
-  //   matrix.setCursor(2, 5);    // start at top left, with one pixel of spacing
-  //   matrix.setTextSize(1);     // size 1 == 8 pixels high
-  //   matrix.setTextWrap(false); // Don't wrap at end of line - will do ourselves
+    matrix.setCursor(5, 12);
+    matrix.setTextSize(1);
+    matrix.setTextWrap(false);
+    matrix.setTextColor(matrix.Color333(7, 2, 2));
+    matrix.print(humDHT11);
+    matrix.println("%");
+    matrix.println("LOW!");  
+    delay(2000);
 
-  //   matrix.setTextColor(matrix.Color333(7, 2, 2));
-  //   matrix.println("%");
-  //   matrix.println("LOW");
-  
-  //   delay(3000);
-  //     matrix.fillRect(0, 0, 32, 32, matrix.Color333(0, 0, 0));
+    matrix.fillRect(0, 0, 32, 32, matrix.Color333(0, 0, 0));
+    matrix.setCursor(2, 2); 
+    matrix.setTextSize(1); 
+    matrix.setTextWrap(false);
+    matrix.setTextColor(matrix.Color333(7, 2, 2));
+    matrix.println("plug");
+    matrix.println(" in");
+    matrix.println("vapor");  
+    delay(2000);
 
-  // }
+// ideally, when it's plugged in: 
+    for (int drop = 0; drop < 31; drop++) {
+      matrix.fillRect(0, 0, 32, drop, matrix.Color333(0, 6, 6));
+      // delay(100);
+      int randomVertical = random(0, 32);
+      matrix.drawLine(randomVertical, random(0, 32) + drop, randomVertical, random(0, 32), matrix.Color333(0, 3, 3));
+      matrix.drawLine(randomVertical, random(0, 32) + drop, randomVertical, random(0, 32), matrix.Color333(0, 2, 4));
+      matrix.drawLine(randomVertical, random(0, 32) + drop, randomVertical, random(0, 32), matrix.Color333(0, 0, 0));
+      matrix.drawLine(randomVertical, random(0, 32) + drop, randomVertical, random(0, 32), matrix.Color333(0, 1, 4));
+      matrix.drawLine(randomVertical, random(0, 32) + drop, randomVertical, random(0, 32), matrix.Color333(0, 7, 7));
+    }
+    // Ilde == screen is black
+    matrix.fillRect(0, 0, 32, 32, matrix.Color333(0, 0, 0));
+  } 
+  // else if ( .... it's running....){
 
+  // } 
+    // else if ( .... it's getting low ....){
 
-  // for (int drop = 0; drop < 31; drop++) {
-  //   matrix.fillRect(0, 0, 32, drop, matrix.Color333(0, 6, 6));
-  //   //    delay(100);
-  //   int randomVertical = random(0, 32);
-  //   matrix.drawLine(randomVertical, random(0, 32) + drop, randomVertical, random(0, 32), matrix.Color333(0, 3, 3));
-  //   matrix.drawLine(randomVertical, random(0, 32) + drop, randomVertical, random(0, 32), matrix.Color333(0, 2, 4));
-  //   matrix.drawLine(randomVertical, random(0, 32) + drop, randomVertical, random(0, 32), matrix.Color333(0, 0, 0));
-  //   matrix.drawLine(randomVertical, random(0, 32) + drop, randomVertical, random(0, 32), matrix.Color333(0, 1, 4));
-  //   matrix.drawLine(randomVertical, random(0, 32) + drop, randomVertical, random(0, 32), matrix.Color333(0, 7, 7));
-  // }
-
-  // // draw some text!
-  // // matrix.fillRect(0, 0, 32, 32, matrix.Color333(0, 0, 0));
-  // matrix.setCursor(2, 5);    // start at top left, with one pixel of spacing
-  // matrix.setTextSize(1);     // size 1 == 8 pixels high
-  // matrix.setTextWrap(false); // Don't wrap at end of line - will do ourselves
-
-  // matrix.setTextColor(matrix.Color333(7, 2, 2));
-  // matrix.println("WATER");
-  // matrix.println("FAIL");
-  // // matrix.println("cables");
-  // delay(1000);
-  // // matrix.fillRect(0, 0, 32, 32, matrix.Color333(0, 0, 0));
-
-  // // matrix.setCursor(2, 5);    // start at top left, with one pixel of spacing
-  // // matrix.println("turn");
-  // // matrix.println("on the");
-  // // matrix.println("thing");
-  // delay(1000);
+  // } 
+  else {
+    // it's ok == idle
+    digitalWrite(mosPin, 0);    
+    digitalWrite(ledPin, LOW);
+    digitalWrite(fanPin, LOW);
+    matrix.fillRect(0, 0, 32, 32, matrix.Color333(0, 0, 1));
+  }
 }
